4dcdcca6ac0ca33b7fe73ab4c6eed3dbc1c01f74
Denis <fominykhdenis@gmail.com>
2018-09-03 22:35:03 +0300

    Change js

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,1 @@
-function include(url) {
-	let script = document.createElement('script');
-	script.src = url;
-	document.getElementsByTagName('head')[0].appendChild(script);
-}
 
-include(`src/js/modules/Slider/Slider.js`);
